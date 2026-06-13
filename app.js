2d955728172f78f0b0c4ee89e6ef2d047fc6e9e1
Edbert C Angwyn <edbert.ca05@gmail.com>
2026-06-13 22:12:53 +1000

    Add debug logs

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ const campgroundsRouters = require('./routes/campground');
 const reviewsRoutes = require('./routes/review');
 const { MongoStore } = require('connect-mongo');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("DB_URL exists:", !!process.env.DB_URL);
+console.log("Node version:", process.version);
 mongoose.connect(dbUrl)
 .then(() => {
         console.log("MONGO CONNECTION OPEN")
